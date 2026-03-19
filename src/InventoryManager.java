052b23cdbd6efc83aedd8b42c83902f39d5b1e7d
Cris <crisjavi1307@gmail.com>
2026-03-18 19:13:00 -0600

    Creacion de la estructura del InventoryManager

--- src/InventoryManager.java
+++ src/InventoryManager.java
@@ -1,3 +1,45 @@
+import java.util.*;
+
 public class InventoryManager {
     
+    private Map<String, String> inventory;
+
+    public InventoryManager(Map<String, String> inventory) {
+        this.inventory = inventory;
+        loadInventory();
+    }
+
+    private void loadInventory() {
+        String[] data = ReadFile.readLines("ListadoProducto.txt");
+
+        for (String line : data) {
+            String[] parts = line.split("\\|");
+
+            String category = parts[0];
+            String product = parts[1];
+
+            inventory.put(product, category);
+        }
+    }
+
+    public void showCategory(String product) {
+        if (inventory.containsKey(product)) {
+            System.out.println("Categoría " + inventory.get(product));
+        } else {
+            System.out.println("Producto no encontrado");
+        }
+    }
+
+    public void showInventory() {
+        inventory.entrySet().stream().sorted(Map.Entry.comparingByValue())
+            .forEach(e -> System.out.println(e.getKey() + "|" + e.getValue()));
+    }
+
+    public boolean hasProduct(String product) {
+        return inventory.containsKey(product);
+    }
+
+    public String getCategory(String product) {
+        return inventory.get(product);
+    }
 }
